79b60c9560676d9cb097b7269ed8cdf17a0ec93e
David Henry <dw_henry@yahoo.com.au>
2016-04-25 15:03:08 +0100

    fixup! Import and view Twilio call data

--- app/importers/daily_calls/twilio.rb
+++ app/importers/daily_calls/twilio.rb
@@ -1,7 +1,7 @@
 module DailyCalls
   module Twilio
     autoload :CallRecord, 'daily_calls/twilio/call_record'
-    autoload :Importeer, 'daily_calls/twilio/importer'
+    autoload :Importer, 'daily_calls/twilio/importer'
     autoload :Retriever, 'daily_calls/twilio/retriever'
     autoload :Saver, 'daily_calls/twilio/saver'
   end

71e69bb1d2685516831644d173881190cc718dc1
David Henry <dw_henry@yahoo.com.au>
2016-04-27 15:06:39 +0100

    Move to use foreman and puma to run web server

---
+++ config/puma.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env puma
+
+rackup DefaultRackup
+
+environment ENV['RACK_ENV'] || 'development'
+daemonize false
+
+pidfile 'tmp/pids/puma.pid'
+state_path 'tmp/pids/puma.state'
+
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads Integer(ENV['MAX_THREADS'] || 5), Integer(ENV['MAX_THREADS'] || 5)
+
+bind 'unix://tmp/sockets/puma.sock'
+port ENV['PORT'] || 3000
+
+preload_app!
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end

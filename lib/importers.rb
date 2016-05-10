8413abb956b2c867ba13c86ce594773d4d730b60
David Henry <dw_henry@yahoo.com.au>
2016-05-10 15:06:09 +0100

    Remove Google WhereDidYouHear importer
    This was used for a one off data import only

--- lib/importers.rb
+++ lib/importers.rb
@@ -1,4 +1,3 @@
 module Importers
   autoload :DailyCalls, 'importers/daily_calls'
-  autoload :WhereDidYouHearAboutUs, 'importers/where_did_you_hear_about_us'
 end
--- lib/importers/where_did_you_hear_about_us.rb
+++
@@ -1,5 +0,0 @@
-module Importers
-  module WhereDidYouHearAboutUs
-    autoload :Google, 'importers/where_did_you_hear_about_us/google'
-  end
-end

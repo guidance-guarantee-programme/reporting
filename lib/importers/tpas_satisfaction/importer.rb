e156c879dfdcace0c9c5f797539f66e42a0518f6
David Henry <dw_henry@yahoo.com.au>
2016-06-08 11:47:57 +0100

    fixup! Import TPAS Satisfaction data

--- lib/importers/tpas_satisfaction/importer.rb
+++ lib/importers/tpas_satisfaction/importer.rb
@@ -15,7 +15,7 @@ module Importers
       def import
         @retriever.process_emails do |email|
           records = @record.build(io: email.file)
-          records && @saver.new(records: records).save
+          @saver.new(records: records).save
         end
       end
     end

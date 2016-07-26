983d1e2eac040b6b1161ab4b4c3628eae4df3fa5
David Henry <dw_henry@yahoo.com.au>
2016-07-26 15:39:22 +0100

    Create costs tables in Database

---
+++ db/migrate/20160721135856_create_costs.rb
@@ -0,0 +1,13 @@
+class CreateCosts < ActiveRecord::Migration
+  def change
+    create_table :costs do |t|
+      t.references :cost_item, index: true, foreign_key: true
+      t.string :month
+      t.integer :value_delta, default: 0, null: false
+      t.references :user, index: true, foreign_key: true
+      t.boolean :forecast, default: false, null: false
+
+      t.timestamps null: false
+    end
+  end
+end

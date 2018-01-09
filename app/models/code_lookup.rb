2f0eb568d8af4857aa2171a773c46862cabac282
Ben Lovell <benjamin.lovell@gmail.com>
2018-01-09 09:42:57 +0000

    Map incorrect entries from 'Other' text fields
    These have now been removed but several immutable entries exist in the
    survey responses. Thus we have to map those to the 'Other' field as a
    special case.

--- app/models/code_lookup.rb
+++ app/models/code_lookup.rb
@@ -1,10 +1,21 @@
 class CodeLookup < ApplicationRecord
   class MissingMappingError < StandardError; end
 
+  CORRECTIONS = [
+    'online booking',
+    'Various',
+    'Age UK',
+    'Macmillan ',
+    'Friend',
+    'Leaflet in job centre'
+  ].freeze
+
   validates :from, presence: true, uniqueness: true
 
   def self.for(value:)
     return '' if value.blank?
+    return 'Other' if CORRECTIONS.include?(value)
+
     find_or_create_by!(from: value).to
   end
 
